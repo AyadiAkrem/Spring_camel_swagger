83f602b96aee51cf2473015a29904afc1d689678
AyadiAkrem <32717598+AyadiAkrem@users.noreply.github.com>
2018-07-15 12:37:16 +0200

    Implement Camel with Spring Boot

--- com.itdeep.eventSearcher/src/main/java/com/itdeep/com/itdeep/eventSearcher/router/EventRouter.java
+++ com.itdeep.eventSearcher/src/main/java/com/itdeep/com/itdeep/eventSearcher/router/EventRouter.java
@@ -8,7 +8,11 @@ public class EventRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("timer:foo")
-          .to("log:bar");
+        from("timer:hello?period={{timer.period}}").routeId("hello").routeGroup("hello-group")
+                .transform().method("myBean", "saySomething")
+                .filter(simple("${body} contains 'foo'"))
+                .to("log:foo")
+                .end()
+                .to("stream:out");
     }
 }
--- com.itdeep.eventSearcher/src/test/java/com/itdeep/com/itdeep/eventSearcher/ApplicationTests.java
+++ com.itdeep.eventSearcher/src/test/java/com/itdeep/com/itdeep/eventSearcher/ApplicationTests.java
@@ -1,16 +1,31 @@
 package com.itdeep.com.itdeep.eventSearcher;
 
+import java.util.concurrent.TimeUnit;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.NotifyBuilder;
+import org.apache.camel.test.spring.CamelSpringBootRunner;
+import org.apache.camel.test.spring.EnableRouteCoverage;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(CamelSpringBootRunner.class)
+@SpringBootTest(classes = Application.class)
+@EnableRouteCoverage
 public class ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private CamelContext camelContext;
+
+    @Test
+    public void shouldProduceMessages() throws Exception {
+        // we expect that one or more messages is automatic done by the Camel
+        // route as it uses a timer to trigger
+        NotifyBuilder notify = new NotifyBuilder(camelContext).whenDone(1).create();
+
+        assertTrue(notify.matches(10, TimeUnit.SECONDS));
+    }
 
 }
